3f0870a68af623a886bba3307d580720765fe013
MarkOsipenko <mark@osipenko.in.ua>
2016-05-23 23:13:41 +0300

    add room controller

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,12 @@
+class RoomsController < ApplicationController
+ before_action :authenticate_user!
+
+ def show
+   
+ end
+
+
+  protected
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: 'users#show'
 
-  devise_for :users
+  devise_for :users, path:'accounts'
+
+  resources :users do
+    resources :rooms
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
