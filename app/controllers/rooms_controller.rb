8bea4d35ca25cce940b9c91077738c9593e2a91d
MarkOsipenko <mark@osipenko.in.ua>
2016-05-23 23:56:31 +0300

    add simple display room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,19 @@
 class RoomsController < ApplicationController
- before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_action :find_room, only: :show
+  before_action :room_params, only: :create
 
- def show
-
- end
+  def show
+  end
 
   protected
+
+    def find_room
+      @room = current_user.rooms.find(params[:id])
+    end
+
+    def room_params
+      params.require(:room).permit(:name, :users, :owner, :messages)
+    end
 
 end
