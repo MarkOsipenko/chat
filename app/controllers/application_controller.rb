cf473eeb65e9c953288a39df647bfbff7e73f604
MarkOsipenko <mark@osipenko.in.ua>
2016-05-23 04:07:15 +0300

    add logout button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :authenticate_user!
 
   protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
-        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
     end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  # before_action :user_params, only: :show
+
+  def show
+  end
+
+  protected
+  #
+  # def user_params
+  #   params.require(:current_user).permit(:name, :email)
+  # end
+
+end
