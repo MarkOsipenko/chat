68e796add7c72186f3e1e3a7738eb48e923c0ff0
MarkOsipenko <mark@osipenko.in.ua>
2016-05-23 11:00:57 +0300

    add first tests for messages

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :message do
-    
+    body "Hello world"
+    user
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Message, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create :user}
+  let(:message)  { create :message, user: user }
+
+  context "resrond" do
+    subject { message }
+    it { is_expected.to respond_to(:body) }
+    it { is_expected.to respond_to(:user_id) }
+  end
 end
