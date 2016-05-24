e41a926cbae2daa0dea1db4f624172a23c1e633d
MarkOsipenko <mark@osipenko.in.ua>
2016-05-25 00:03:05 +0300

    remove ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  before_action :all_rooms, only: :show
+
 
   def show
     @rooms = current_user.rooms
+  end
+
+  protected
+
+  def all_rooms
+    @all_rooms = Room.all
   end
 
 end
