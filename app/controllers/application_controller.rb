ba6b73238f78482c73176ed33443bfde3425ece8
MarkOsipenko <mark@osipenko.in.ua>
2016-05-25 17:26:45 +0300

    add uploader Avatar, display avatar in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,9 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
-    end
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password,
+      :password_confirmation, :remember_me, :avatar, :avatar_cache) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password,
+      :password_confirmation, :current_password, :avatar, :avatar_cache) }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,4 +11,9 @@ class User < ApplicationRecord
   has_many :messages
   has_many :users_rooms
   has_many :rooms, through: :users_rooms
+
+  validates_presence_of   :avatar
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
+
 end
