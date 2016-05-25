8bad3738c113a26c2380960f0b47bdf937a65ea7
MarkOsipenko <mark@osipenko.in.ua>
2016-05-25 11:29:55 +0300

    add tests on create room method

--- spec/requests/user-room_relation_spec.rb
+++ spec/requests/user-room_relation_spec.rb
@@ -31,6 +31,22 @@ RSpec.describe "User in rooms", type: :request do
         it { is_expected.not_to have_content("Disconnect from:")}
         it { expect(find(".new_rooms")).to have_link(harrys_room.name) }
       end
+
+    end
+
+    context "creare room" do
+      context "valid" do
+        before do
+          fill_in "Room name", with: "Awesome room"
+          click_button "Add room"
+        end
+        it { is_expected.to have_link("Awesome room") }
+      end
+
+      context "invalid" do
+        before { fill_in "Room name", with: "" }
+        it { expect{ click_button "Add room" }.to change(Room, :count).by(0) }
+      end
     end
   end
 end
