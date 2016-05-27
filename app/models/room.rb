b1bfc5de628ebc29d383e403e974662e52b1a8eb
MarkOsipenko <mark@osipenko.in.ua>
2016-05-27 12:24:50 +0300

    room-message-dependency destroy

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   has_many :users_rooms
   has_many :users, through: :users_rooms
-  has_many :messages
+  has_many :messages, dependent: :destroy 
 
   validates :name, presence: true
   validates :owner_id, presence: true
