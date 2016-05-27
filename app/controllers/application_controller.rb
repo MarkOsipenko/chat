2228ceebbf345ca63a71a458be82f77d45ef99a1
MarkOsipenko <mark@osipenko.in.ua>
2016-05-27 10:45:33 +0300

    add validation on uniqueness user rooms, fix issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def configure_permitted_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password,
       :password_confirmation, :remember_me, :avatar, :avatar_cache) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password,
